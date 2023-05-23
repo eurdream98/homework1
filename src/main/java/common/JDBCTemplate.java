bef89a52423ecaf0c82fced7f399d157c364f3ed
고동환\eurdr <eurdream98@naver.com>
2023-05-23 14:41:46 +0900

    Initial commit

---
+++ src/main/java/common/JDBCTemplate.java
@@ -0,0 +1,79 @@
+//JDBCTemplate
+
+package common;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileReader("src/main/java/config/connection-info.properties"));
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url, prop);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+    public static void close(Connection con) {
+
+        try {
+            if(con != null & !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void close(Statement stmt) {
+
+        try {
+            if(stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void close(ResultSet rset) {
+
+        try {
+            if(rset != null && !rset.isClosed()) {
+                rset.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/main/java/section02/preparedstatement/Application3.java
@@ -0,0 +1,55 @@
+package section02.preparedstatement;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+        /* 1. Connection 생성 */
+        Connection con  = getConnection();
+
+        /* 2. PreparedStatement, ResultSet 생성 */
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+
+        /* 3. Scanner를 활용하여 조회할 사번 입력받기 */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("조회할 사번을 입력해 주세요. : ");
+        String empId = sc.nextLine();
+
+        /* 4. 조회를 위한 쿼리 작성 (placeholder 사용) */
+        String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ?";
+
+        try {
+            /* 5. prepareStatement 생성 및 쿼리 준비 */
+            pstmt = con.prepareStatement(query);
+
+            /* 6. 조건에 해당하는 사번 세팅 */
+            pstmt.setString(1, empId);
+
+            /* 7. ResultSet에 존재하는 결과값을 출력 */
+            rset = pstmt.executeQuery();
+            while (rset.next()) {
+                System.out.println("MANAGER_ID: " + rset.getString("MANAGER_ID"));
+                System.out.println("EMP_NAME: " + rset.getString("EMP_NAME"));
+
+
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            /* 8. 자원 반납 */
+            close(con);
+            close(pstmt);
+            close(rset);
+        }
+    }
+}
